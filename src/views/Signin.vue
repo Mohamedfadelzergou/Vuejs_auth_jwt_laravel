fd1dc17b552b6f4d7da453b72049a9916f02af9a
Mohamedfadelzergou <mohamedfadelzergou@gmail.com>
2022-01-04 23:53:18 +0100

    Sending Client Request to API with Axios

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -2,14 +2,14 @@
   <h1>Signin</h1>
   <div class="row">
     <div class="col-md-6 mx-auto mt-3">
-      <form action="">
+      <form @submit.prevent="signin">
         <div class="form-group mb-2">
           <label for="Email">Email</label>
-          <input type="Email" class="form-control" name="Email" id="Email">
+          <input type="Email" v-model="user.email" class="form-control" name="Email" id="Email">
         </div>
         <div class="form-group mb-2">
           <label for="Password">Password</label>
-          <input type="Password" class="form-control" name="Password" id="Password">
+          <input type="Password" v-model="user.password" class="form-control" name="Password" id="Password">
         </div>
         <button class="btn btn-primary w-100">Singn in</button>
       </form>
@@ -18,7 +18,21 @@
 </template>
 
 <script>
-
+import axios from 'axios';
 export default {
+  data(){
+    return{
+      user:{
+        email:'',
+        password:''
+      }
+    }
+  },
+  methods:{
+    async signin(){
+      const response= await axios.post('http://localhost:8000/api/auth/login',this.user)
+      console.log(response.data)
+    }
+  }
 }
 </script>
